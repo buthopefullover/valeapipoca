22fb8cfe74a2d3061bdc1052b05574cd081c5cd3
victoria <victoria.set.1999@gmail.com>
2022-07-04 21:40:39 -0300

    mais tentativas

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
@@ -7,7 +6,7 @@ const port = process.env.PORT || 8080;
 // Connection to the database
 const { Client } = require('pg');
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgres://feqbdwlhikczsq:ed27e3a787ef7424281e1c4159c7ae17af1af0a05d3a4f0b4e3253832dba1db6@ec2-23-23-151-191.compute-1.amazonaws.com:5432/d9kgl81b68kl2n',
   ssl: {
     rejectUnauthorized: false
   }
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // the single Axios instance we use for calls
 const apiClient = axios.create({
   //baseURL: 'http://localhost:5000',
-  baseURL: process.env.DATABASE_URL, // with ssl?
+  baseURL: 'postgres://feqbdwlhikczsq:ed27e3a787ef7424281e1c4159c7ae17af1af0a05d3a4f0b4e3253832dba1db6@ec2-23-23-151-191.compute-1.amazonaws.com:5432/d9kgl81b68kl2n', // with ssl?
   withCredentials: false, // this is the default
   headers: {
     Accept: 'application/json',
