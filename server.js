341c5939cc78468a4ad6277042538ce10e45d222
victoria <victoria.set.1999@gmail.com>
2022-07-06 20:02:03 -0300

    tentativas

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require("express");
 // const history = require("connect-history-api-fallback")
 // const serveStatic = require("serve-static");
 // const path = require("path");
-const port = 30000 || 8081;
+const port = process.env.PORT || 8081;
 
 // Connection to the database
 const { Client } = require('pg');
 const client = new Client({
-    host: 'ec2-23-23-151-191.compute-1.amazonaws.com',
-    port: 5432,
-    user: 'feqbdwlhikczsq',
-    password: 'ed27e3a787ef7424281e1c4159c7ae17af1af0a05d3a4f0b4e3253832dba1db6',
-    database: 'd9kgl81b68kl2n',
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     ssl: {
         rejectUnauthorized: false
     }
