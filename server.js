50a1321452fed628f2c18601bb346e6b2cd84520
victoria <victoria.set.1999@gmail.com>
2022-07-06 21:29:56 -0300

    env variables

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const port = 30000 || 8081;
 // Connection to the database
 const { Client } = require('pg');
 const client = new Client({
-    host: 'ec2-23-23-151-191.compute-1.amazonaws.com',
-    port: 5432,
-    user: 'feqbdwlhikczsq',
-    password: 'ed27e3a787ef7424281e1c4159c7ae17af1af0a05d3a4f0b4e3253832dba1db6',
-    database: 'd9kgl81b68kl2n',
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     ssl: {
         rejectUnauthorized: false
     }
